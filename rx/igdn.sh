24ebc2e47f80d3efd873765d5133dfec53477f09
Gallo Chingon <9777026+gallo-s-chingon@users.noreply.github.com>
2024-05-29 02:26:34 -0700

    update zshrc history-ignore globs

--- rx/igdn.sh
+++ rx/igdn.sh
@@ -2,11 +2,11 @@
 export PATH=/opt/homebrew/bin/:$PATH
 igdown () {
     cd /Volumes/armor/didact/IG
-    instaloader -l gallo.s.chingon --no-video-thumbnails --no-profile-pic --no-captions --no-metadata-json --latest-stamps /Users/gchingon/.config/instaloader/latest-stamps.ini ignorancebegone
-    instaloader -l gallo.s.chingon --no-video-thumbnails --no-profile-pic --no-captions --no-metadata-json --latest-stamps /Users/gchingon/.config/instaloader/latest-stamps.ini paths2frdm2022
-    instaloader -l gallo.s.chingon --no-video-thumbnails --no-profile-pic --no-captions --no-metadata-json --latest-stamps /Users/gchingon/.config/instaloader/latest-stamps.ini receiptdropper
-    instaloader -l gallo.s.chingon --no-video-thumbnails --no-profile-pic --no-captions --no-metadata-json --latest-stamps /Users/gchingon/.config/instaloader/latest-stamps.ini yusef_el_19
-    instaloader -l gallo.s.chingon --no-video-thumbnails --no-profile-pic --no-captions --no-metadata-json --latest-stamps /Users/gchingon/.config/instaloader/latest-stamps.ini amyr_law
+    instaloader --no-video-thumbnails --no-profile-pic --no-captions --no-metadata-json --latest-stamps /Users/gchingon/.config/instaloader/latest-stamps.ini ignorancebegone
+    instaloader --no-video-thumbnails --no-profile-pic --no-captions --no-metadata-json --latest-stamps /Users/gchingon/.config/instaloader/latest-stamps.ini paths2frdm2022
+    instaloader --no-video-thumbnails --no-profile-pic --no-captions --no-metadata-json --latest-stamps /Users/gchingon/.config/instaloader/latest-stamps.ini receiptdropper
+    instaloader --no-video-thumbnails --no-profile-pic --no-captions --no-metadata-json --latest-stamps /Users/gchingon/.config/instaloader/latest-stamps.ini yusef_el_19
+    instaloader --no-video-thumbnails --no-profile-pic --no-captions --no-metadata-json --latest-stamps /Users/gchingon/.config/instaloader/latest-stamps.ini amyr_law
 }
 
 igdown
